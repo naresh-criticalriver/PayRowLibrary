66de9ced9759a456c36bee86ea2330519128e871
Naresh <koppera.kumar@tigerspike.com>
2022-05-09 12:29:20 +0530

    updated MainActivity.kt

--- app/src/main/java/com/example/payrowapp/MainActivity.kt
+++ app/src/main/java/com/example/payrowapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.payrowapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.payrowlibrary.DashboardActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DashboardActivity().showToast("Hello World")
     }
 }

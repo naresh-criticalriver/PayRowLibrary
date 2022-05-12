529c07ff03c926bc4a141e709abc48d531ff8e57
Naresh <koppera.kumar@tigerspike.com>
2022-05-12 16:18:18 +0530

    removed ImagePreview.kt and added Utils.kt

---
+++ PayRowLibrary/src/main/java/com/example/payrowlibrary/Utils.kt
@@ -0,0 +1,11 @@
+package com.example.payrowlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class Utils(val context: Context) {
+
+    fun  showToast(msg: String){
+        Toast.makeText(context,msg, Toast.LENGTH_LONG).show()
+    }
+}
--- app/src/main/java/com/example/payrowapp/MainActivity.kt
+++ app/src/main/java/com/example/payrowapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.payrowapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.payrowlibrary.DashboardActivity
+import com.example.payrowlibrary.Utils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DashboardActivity().showToast("Hello World")
+        Utils(this).showToast("Welcome")
     }
 }
